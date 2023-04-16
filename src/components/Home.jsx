fd6137a8bb7673be64977813786d62e39dab47f3
Abhishek Panwar <imabhishek612@gmail.com>
2023-04-16 22:40:40 +0530

    fixed error and made boxes with img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Container } from '@chakra-ui/react';
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
@@ -11,10 +11,22 @@ import img3 from '../assets/3.jpg';
 import img4 from '../assets/4.jpg';
 import img5 from '../assets/5.png';
 
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%,-50%)',
+  textTransform: 'uppercase',
+  p: '4',
+};
+
 const Home = () => {
   return (
     <Box>
       <MyCarousel />
+      <Container maxW={'container.xl'} maxH={'100vh'} p={'16'}>
+        Lucky
+      </Container>
     </Box>
   );
 };
@@ -31,15 +43,29 @@ const MyCarousel = () => {
     >
       <Box w={'full'} h={'100vh'}>
         <Image src={img1} />
-        <Heading bgColor={'blackAlpha.600'} color={'white'}>
+        <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
           Watch the Future
         </Heading>
       </Box>
 
       <Box w={'full'} h={'100vh'}>
         <Image src={img2} />
-        <Heading bgColor={'blackAlpha.600'} color={'white'}>
-          Future is AI
+        <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+          Future is Gaming
+        </Heading>
+      </Box>
+
+      <Box w={'full'} h={'100vh'}>
+        <Image src={img3} />
+        <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+          Gaming on console
+        </Heading>
+      </Box>
+
+      <Box w={'full'} h={'100vh'}>
+        <Image src={img4} />
+        <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+          Night life is cool
         </Heading>
       </Box>
     </Carousel>
